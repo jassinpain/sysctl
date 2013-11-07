7572832034a392196f61b7882a9ab881d18f8c21
Joshua Miller <joshua.miller@fitsnips.net>
2013-11-07 14:07:15 -0800

    Fixing error on upload with private chef. ERROR: Chef::Exceptions::InvalidCookbookVersion: '6' does not match 'x.y.z' or 'x.y'

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'cookbooks@onehealth.com'
 license          'Apache v2.0'
 description      'Configures sysctl parameters'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.4'
+version          '0.3.5'
 supports 'ubuntu', '>= 10.04'
 supports 'centos', '>= 5.9'
-supports 'scientific', '>= 6'
+supports 'scientific', '>= 6.0'
 %w(debian redhat pld).each do |os|
   supports os
 end
